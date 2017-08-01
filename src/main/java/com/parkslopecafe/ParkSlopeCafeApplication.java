fc41706aa6d75d9d8089857238ac96b0c7147ca2
David Alviola <david.alviola@gmail.com>
2017-07-31 22:18:08 -0500

    Renamed main application class name. Added information to properties.dist.

--- src/main/java/com/parkslopecafe/ParkSlopeCafeApplication.java
+++ src/main/java/com/parkslopecafe/ParkSlopeCafeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class ParkSlopeCafeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(ParkSlopeCafeApplication.class, args);
 	}
 }
