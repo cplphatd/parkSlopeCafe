8783d70b34e4a97149a120e3d9283cd2572ba3f3
David Alviola <david.alviola@gmail.com>
2017-08-31 22:14:57 -0500

    Added hamburger navigation bar to all console directory pages. Updated seeder file.

--- src/main/resources/static/sql/seeder.sql
+++ src/main/resources/static/sql/seeder.sql
@@ -3,3 +3,8 @@ INSERT INTO users(username, password) VALUES ('admin', /*'paste hashed password 
 INSERT INTO user_roles(role, user_id) VALUES ('ROLE_ADMIN', 1);
 
 INSERT INTO store_status(store_is_open, decoration, message) VALUES (true, 'none', null);
+
+INSERT INTO beers(category, description, is_in_stock, name, picture) VALUES
+  ('Ale', 'Strong dark ale', true, 'Patrasche', 'https://cdn.filestackcontent.com/f4MsJxXrReSxIolK0Lpp'),
+  ('Lambic', 'Red Ale', true, 'Duchesse de Bourgogne', 'https://cdn.filestackcontent.com/ESXcpLa4RgG9h4PQiAgE'),
+  ('Fruit Beer', 'Bitter and lightly sweet', true, 'Framboise Boon', 'https://cdn.filestackcontent.com/b99UGNwcQj2BHpaq2xzB');
