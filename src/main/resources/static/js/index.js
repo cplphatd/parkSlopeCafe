57fe39f195b0d1f3bbb092d2b1b17fa18b1a9f28
David Alviola <david.alviola@gmail.com>
2017-09-09 19:55:14 -0500

    Added beer info overlays. Added media queries for better behavior on larger screens. Implemented decoration messages.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -15,27 +15,49 @@ $(document).ready(function() {
                carouselIndicators += "<li data-target='#myCarousel' data-slide-to='" + i + "' class='active'></li>";
                carouselInner += "<div class='item active'>\n" +
                    "                    <div class='container'>\n" +
-                   "                        <span class='beerName'>" + beerList[i].name + "</span>" +
-                   "                        <a href='#'>\n" +
-                   "                            <img class='beerPic' src='" + beerList[i].picture + "'/>\n" +
-                   "                        </a>\n" +
+                   "                        <h5 class='beerName'>" + beerList[i].name + "</h5>" +
+                   "                        <img class='beerPic' src='" + beerList[i].picture + "'/>\n" +
+                   "                        <button class='btn btn-secondary pscBtn my_popup" + i + "_open'>インフォ</button>" +
+                   "                        <div id='my_popup" + i + "'>" +
+                   "                            <h2>" + beerList[i].name + "</h2>" +
+                   "                            <p>カテゴリー：" + beerList[i].category + "</p>" +
+                   "                            <p>デスクリプション：" + beerList[i].description + "</p>" +
+                   "                            <p>アベイラブル：" + beerList[i].inStock + "</p>" +
+                   "                        </div>" +
                    "                    </div>\n" +
-                   "                </div>"
+                   "                </div>";
            } else {
                carouselIndicators += "<li data-target='#myCarousel' data-slide-to='" + i + "' class=''></li>";
                carouselInner += "<div class='item'>\n" +
                    "                    <div class='container'>\n" +
-                   "                        <span class='beerName'>" + beerList[i].name + "</span>" +
-                   "                        <a href='#'>\n" +
-                   "                            <img class='beerPic' src='" + beerList[i].picture + "'/>\n" +
-                   "                        </a>\n" +
+                   "                        <h5 class='beerName'>" + beerList[i].name + "</h5>" +
+                   "                        <img class='beerPic' src='" + beerList[i].picture + "'/>\n" +
+                   "                        <button class='btn btn-secondary pscBtn my_popup" + i + "_open'>インフォ</button>" +
+                   "                        <div id='my_popup" + i + "'>" +
+                   "                            <h2>" + beerList[i].name + "</h2>" +
+                   "                            <p>カテゴリー：" + beerList[i].category + "</p>" +
+                   "                            <p>デスクリプション：" + beerList[i].description + "</p>" +
+                   "                            <p>アベイラブル：" + beerList[i].inStock + "</p>" +
+                   "                        </div>" +
                    "                    </div>\n" +
-                   "                </div>"
+                   "                </div>";
            }
        }
 
        $(".carousel-indicators").html(carouselIndicators);
        $(".carousel-inner").html(carouselInner);
+
+       //Initialize all the popups
+       for (var j = 0; j < beerList.length; j += 1) {
+           var beerPopupId = "#my_popup" + j;
+           $(beerPopupId).popup({
+               color: 'black',
+               opacity: 1,
+               transition: 'all 0.3s',
+               scrolllock: true
+           });
+       }
+
    });
 
    request.fail(function() {
@@ -45,5 +67,15 @@ $(document).ready(function() {
    request.always(function() {
        console.log("Beer list ajax complete.");
    });
+
+    //Smooth scroll event listener
+    $(document).on("click", ".anchorNav", function(event) {
+        event.preventDefault();
+
+        var nav =
+        $("html, body").animate({
+            scrollTop: $($.attr(this, "href")).offset().top
+        }, 500);
+    });
 
 });
